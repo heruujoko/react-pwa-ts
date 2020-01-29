c294288e9a8c8858e9379e828cb05d82a1fc68cf
Heru Joko Utomo <heru@wallextech.com>
2020-01-29 11:01:13 +0800

    header interceptor config support

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,8 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import './App.css';
-import todoService from './services/todo.service';
 import AppRouter from "./Router";
 
 const App: React.FC = () => {
-
-  const fetchMe = async () => {
-    const resp = await todoService.getTodo(1);
-    console.log('resp', resp);
-  };
-
-  useEffect(() => {
-    fetchMe();
-  }, []);
-
   return (
     <div className="App">
       <AppRouter />
--- src/services/todo.service.ts
+++ src/services/todo.service.ts
@@ -1,9 +1,9 @@
-import request from '../utils/request.utils';
+import request from './api.service';
 import { plainToClass } from 'class-transformer';
 import Todo from "../pojo/Todo";
 
 const getTodo = async (id: number): Promise<Todo> => {
-    const resp = await request.get(`https://jsonplaceholder.typicode.com/todos/${id}`);
+    const resp = await request.get(`/todos/${id}`);
     return plainToClass(Todo, resp);
 };
 
